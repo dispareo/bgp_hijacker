0e72c7abe5151fecef24264b99018dba529a6ded
dispareo <security@dispareo.ch>
2022-10-19 17:34:44 -0500

    Update create_routes.py

--- create_routes.py
+++ create_routes.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 import commands
 
-os.system('vtysh -c "sh run" -c "config t" -c "router bgp 1000" -c "address-family ipv6 unicast" -c "network 2001:2022:3:3::/64" -c "do show run"')
+os.system('vtysh -c "sh run" -c "config t" -c "router bgp 1000" -c "address-family ipv6 unicast" -c "network 2001:2022:3:3::/126" -c "do show run"')
 #output = commands.getoutput(#"vtysh -c "sh run" -c "config t" -c "router bgp 1000" -c "address-family ipv6 unicast" -c "network 2001:2022:3:3::/64" -c "do show run"")
 #subprocess.run("vtysh -c 'config t'")
 #subprocess.run("vtysh -c 'router bgp 1000'")
