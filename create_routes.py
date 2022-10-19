977b685cf7875ea0db10092315cf64445eb7c00d
dispareo <security@dispareo.ch>
2022-10-19 14:11:44 -0500

    Update create_routes.py

--- create_routes.py
+++ create_routes.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python
 
+import os
+
+os.system("vtysh -c 'show running-config'")
+
+os.system("vtysh -c 'router bgp 1000'")
+os.system("vtysh -c 'address-family ipv6 unicast'")
+os.system("vtysh -c 'network 2001:2022:3:3::/64'")
+os.system("vtysh -c 'show running-config'")
