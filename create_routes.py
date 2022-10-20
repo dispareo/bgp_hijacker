efadea102000888b0b5e4ff24c0e806e3c273345
dispareo <security@dispareo.ch>
2022-10-19 23:32:51 -0500

    Update create_routes.py

--- create_routes.py
+++ create_routes.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
-import subprocess
 import os
-#import commands
+import time
 
 os.system('vtysh -c "sh run" -c "config t" -c "router bgp 1000" -c "address-family ipv6 unicast" -c "network 2001:2022:3:3::/126" -c "do show run"')
-#output = commands.getoutput(#"vtysh -c "sh run" -c "config t" -c "router bgp 1000" -c "address-family ipv6 unicast" -c "network 2001:2022:3:3::/64" -c "do show run"")
-#subprocess.run("vtysh -c 'config t'")
-#subprocess.run("vtysh -c 'router bgp 1000'")
-#subprocess.run("vtysh -c 'address-family ipv6 unicast'")
-#subprocess.run("vtysh -c 'network 2001:2022:3:3::/64'")
-#subprocess.run("vtysh -c 'show running-config'")
+#I prefer subprocess over os.system, but all the -c's and quotation weirdness - including having to remain in config t in order for the network commands to work -
+#mandates its usage, along with the author's non-dev background. It isn't pretty, it's functinoal, and fucntional is pretty.
+
+
+array = []
+varhex = "00" # starting hex value
+for i in range(0, 3):      # increment the addresses
+    i = int(varhex, 16)     # define i as the decimal equivalent of varhex
+    i +=4                   # increment i by one
+    print("2001:2022:3:3::" + hex(i)[2:] + "128")  
+    array.append("2001:2022:3:3::" + hex(i)[2:] + "128");    # print out the incremented value, but in hex form
+    varhex = hex(i)
+
+
+
+for i in array:
+    os.system('vtysh -c "sh run" -c "config t" -c "router bgp 1000" -c "address-family ipv6 unicast" -c "network (i)" -c "do show run"')
+    time.sleep(2)
